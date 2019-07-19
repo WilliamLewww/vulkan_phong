1d32fce578051be420a20b6b9784397638b690f1
William Lew <WilliamMilesLew@gmail.com>
2019-07-18 21:21:32 -0700

    initialized vulkan, physical device, and logical device

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,6 +3,8 @@
 void Engine::initialize() {
 	initializeWindow();
 	initializeVulkan();
+	initializePhysicalDevice();
+	initializeLogicalDevice();
 }
 
 void Engine::initializeWindow() {
@@ -18,18 +20,64 @@ void Engine::initializeVulkan() {
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.apiVersion = VK_API_VERSION_1_1;
 
-	GLuint glfwExtensionCount = 0;
-	const char** glfwExtensions;
-	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
 	VkInstanceCreateInfo createInfo = {};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 	createInfo.enabledExtensionCount = glfwExtensionCount;
 	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
+    }
+}
+
+void Engine::initializePhysicalDevice() {
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	physicalDevice = devices[0];
+}
+
+void Engine::initializeLogicalDevice() {
+	uint32_t queueFamilyIndex = -1;
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, queueFamilies.data());
+
+	for (int x = 0; x < queueFamilies.size() && queueFamilyIndex == -1; x++) {
+		if (queueFamilies[x].queueCount > 0 && queueFamilies[x].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			queueFamilyIndex = x;
+		}
+	}
+
+	float queuePriority = 1.0f;
+
+	VkDeviceQueueCreateInfo queueCreateInfo = {};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = queueFamilyIndex;
+	queueCreateInfo.queueCount = 1;
+	queueCreateInfo.pQueuePriorities = &queuePriority;
+
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+	VkDeviceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	createInfo.pQueueCreateInfos = &queueCreateInfo;
+	createInfo.queueCreateInfoCount = 1;
+	createInfo.pEnabledFeatures = &deviceFeatures;
+	createInfo.enabledExtensionCount = 0;
 	createInfo.enabledLayerCount = 0;
 
-	vkCreateInstance(&createInfo, nullptr, &instance);
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &logicalDevice) != VK_SUCCESS) {
+	    throw std::runtime_error("failed to create logical device!");
+	}
+	vkGetDeviceQueue(logicalDevice, queueFamilyIndex, 0, &graphicsQueue);
 }
 
 void Engine::start() {
@@ -39,6 +87,8 @@ void Engine::start() {
 }
 
 void Engine::quit() {
+	vkDestroyDevice(logicalDevice, nullptr);
+	vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 }
--- src/engine.h
+++ src/engine.h
@@ -2,15 +2,25 @@
 
 #pragma once
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <iostream>
 
 class Engine {
 private:
 	GLFWwindow* window;
 
 	VkInstance instance;
+	VkDebugUtilsMessengerEXT debugMessenger;
+
+	VkPhysicalDevice physicalDevice;
+	VkDevice logicalDevice;
+
+	VkQueue graphicsQueue;
 
 	void initializeWindow();
 	void initializeVulkan();
+	void initializePhysicalDevice();
+	void initializeLogicalDevice();
 public:
 	void initialize();
 	void start();
