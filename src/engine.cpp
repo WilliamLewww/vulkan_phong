1a4f922859cc9538c91db27703b14e281588ddd7
William Lew <WilliamMilesLew@gmail.com>
2019-07-19 22:19:42 -0700

    initialized swap chain and image views

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,10 +1,19 @@
 #include "engine.h"
 
+const int SCREENWIDTH = 1000;
+const int SCREENHEIGHT = 600;
+
+const std::vector<const char*> deviceExtensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
 void Engine::initialize() {
 	initializeWindow();
 	initializeVulkan();
 	initializePhysicalDevice();
 	initializeLogicalDevice();
+	initializeSwapChain();
+	initializeImageViews();
 }
 
 void Engine::initializeWindow() {
@@ -12,7 +21,7 @@ void Engine::initializeWindow() {
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	window = glfwCreateWindow(1000, 600, "Vulkan-Phong", nullptr, nullptr);
+	window = glfwCreateWindow(SCREENWIDTH, SCREENHEIGHT, "Vulkan-Phong", nullptr, nullptr);
 }
 
 void Engine::initializeVulkan() {
@@ -49,8 +58,8 @@ void Engine::initializePhysicalDevice() {
 }
 
 void Engine::initializeLogicalDevice() {
-	uint32_t queueFamilyIndex = -1;
-	uint32_t queuePresentIndex = -1;
+	queueFamilyIndex = -1;
+	queuePresentIndex = -1;
 	uint32_t queueFamilyCount = 0;
 	vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyCount, nullptr);
 	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
@@ -90,7 +99,8 @@ void Engine::initializeLogicalDevice() {
 	createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
     createInfo.pQueueCreateInfos = queueCreateInfos.data();
     createInfo.pEnabledFeatures = &deviceFeatures;
-    createInfo.enabledExtensionCount = 0;
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+	createInfo.ppEnabledExtensionNames = deviceExtensions.data();
     createInfo.enabledLayerCount = 0;
 
 	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &logicalDevice) != VK_SUCCESS) {
@@ -100,6 +110,122 @@ void Engine::initializeLogicalDevice() {
 	vkGetDeviceQueue(logicalDevice, queuePresentIndex, 0, &presentQueue);
 }
 
+void Engine::initializeSwapChain() {
+	VkSurfaceFormatKHR format;
+	VkPresentModeKHR presentMode;
+	VkExtent2D extent;
+
+	VkSurfaceCapabilitiesKHR capabilities;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &capabilities);
+
+	uint32_t formatCount = 0;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, nullptr);
+	std::vector<VkSurfaceFormatKHR> formats(formatCount);
+	if (formatCount != 0) {
+	    vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice, surface, &formatCount, formats.data());
+	}
+
+	uint32_t presentModeCount = 0;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, nullptr);
+	std::vector<VkPresentModeKHR> presentModes(presentModeCount);
+	if (presentModeCount != 0) {
+	    vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, surface, &presentModeCount, presentModes.data());
+	}
+
+    for (VkSurfaceFormatKHR& formatFromVector : formats) {
+    	if (formatFromVector.format == VK_FORMAT_B8G8R8A8_UNORM && formatFromVector.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+    		format = formatFromVector;
+    	}
+    }
+
+    VkPresentModeKHR bestMode = VK_PRESENT_MODE_FIFO_KHR;
+    for (VkPresentModeKHR& presentModeFromVector : presentModes) {
+        if (presentModeFromVector == VK_PRESENT_MODE_MAILBOX_KHR) {
+            presentMode = presentModeFromVector;
+        }
+    }
+
+    if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+		extent = capabilities.currentExtent;
+	} 
+	else {
+		VkExtent2D actualExtent = {SCREENWIDTH, SCREENHEIGHT};
+
+		actualExtent.width = std::max(capabilities.minImageExtent.width, std::min(capabilities.maxImageExtent.width, actualExtent.width));
+		actualExtent.height = std::max(capabilities.minImageExtent.height, std::min(capabilities.maxImageExtent.height, actualExtent.height));
+
+		extent = actualExtent;
+	}
+
+	uint32_t imageCount = capabilities.minImageCount + 1;
+    if (capabilities.maxImageCount > 0 && imageCount > capabilities.maxImageCount) {
+        imageCount = capabilities.maxImageCount;
+    }
+
+    uint32_t queueFamilyIndices[] = {queueFamilyIndex, queuePresentIndex};
+
+    VkSwapchainCreateInfoKHR createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    createInfo.surface = surface;
+    createInfo.minImageCount = imageCount;
+    createInfo.imageFormat = format.format;
+    createInfo.imageColorSpace = format.colorSpace;
+    createInfo.imageExtent = extent;
+    createInfo.imageArrayLayers = 1;
+    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+    if (queueFamilyIndex != queuePresentIndex) {
+        createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        createInfo.queueFamilyIndexCount = 2;
+        createInfo.pQueueFamilyIndices = queueFamilyIndices;
+    } else {
+        createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    }
+
+    createInfo.preTransform = capabilities.currentTransform;
+    createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    createInfo.presentMode = presentMode;
+    createInfo.clipped = VK_TRUE;
+
+    createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+    if (vkCreateSwapchainKHR(logicalDevice, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create swap chain!");
+    }
+
+   	vkGetSwapchainImagesKHR(logicalDevice, swapChain, &imageCount, nullptr);
+    swapChainImages.resize(imageCount);
+    vkGetSwapchainImagesKHR(logicalDevice, swapChain, &imageCount, swapChainImages.data());
+
+    swapChainImageFormat = format.format;
+    swapChainExtent = extent;
+}
+
+void Engine::initializeImageViews() {
+	swapChainImageViews.resize(swapChainImages.size());
+
+	for (size_t i = 0; i < swapChainImages.size(); i++) {
+        VkImageViewCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        createInfo.image = swapChainImages[i];
+        createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        createInfo.format = swapChainImageFormat;
+        createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        createInfo.subresourceRange.baseMipLevel = 0;
+        createInfo.subresourceRange.levelCount = 1;
+        createInfo.subresourceRange.baseArrayLayer = 0;
+        createInfo.subresourceRange.layerCount = 1;
+
+        if (vkCreateImageView(logicalDevice, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create image views!");
+        }
+    }
+}
+
 void Engine::start() {
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -107,6 +233,7 @@ void Engine::start() {
 }
 
 void Engine::quit() {
+	vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
 	vkDestroyDevice(logicalDevice, nullptr);
 	vkDestroySurfaceKHR(instance, surface, nullptr);
 	vkDestroyInstance(instance, nullptr);
--- src/engine.h
+++ src/engine.h
@@ -17,13 +17,24 @@ private:
 	VkPhysicalDevice physicalDevice;
 	VkDevice logicalDevice;
 
+	uint32_t queueFamilyIndex;
+	uint32_t queuePresentIndex;
 	VkQueue graphicsQueue;
 	VkQueue presentQueue;
+
+	VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+
+    std::vector<VkImageView> swapChainImageViews;
 
 	void initializeWindow();
 	void initializeVulkan();
 	void initializePhysicalDevice();
 	void initializeLogicalDevice();
+	void initializeSwapChain();
+	void initializeImageViews();
 public:
 	void initialize();
 	void start();
