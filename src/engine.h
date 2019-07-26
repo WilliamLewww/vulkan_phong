3e07c10e7e058616a4ff7b015ab8be2445caad09
William Lew <WilliamMilesLew@gmail.com>
2019-07-25 23:55:09 -0700

    created buffer for textured object

--- src/engine.h
+++ src/engine.h
@@ -3,6 +3,7 @@
 #pragma once
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <stb_image.h>
 #include <fstream>
 #include <vector>
 #include <set>
@@ -53,6 +54,9 @@ private:
 	VkDescriptorPool descriptorPool;
 	std::vector<VkDescriptorSet> descriptorSets;
 
+	VkImage textureImage;
+	VkDeviceMemory textureImageMemory;
+
 	std::vector<VkSemaphore> imageAvailableSemaphores;
 	std::vector<VkSemaphore> renderFinishedSemaphores;
 	std::vector<VkFence> inFlightFences;
@@ -70,6 +74,7 @@ private:
 	void initializeFramebuffers();
 	void initializeCommandPool();
 
+	void initializeTextureImage();
 	void initializeVertexBuffer();
 	void initializeIndexBuffer();
 	void initializeUniformBuffers();
@@ -83,8 +88,16 @@ private:
 	void render();
 	void updateUniformBuffer(uint32_t currentImage);
 
+	void createImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+	void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
+	void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
+
 	std::vector<char> readFile(const std::string& filepath);
 	VkShaderModule createShaderModule(const std::vector<char>& code);
+	
+	VkCommandBuffer beginSingleTimeCommands();
+	void endSingleTimeCommands(VkCommandBuffer commandBuffer);
+
 	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
 	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
 	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
